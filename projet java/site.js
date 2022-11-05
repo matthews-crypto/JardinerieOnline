8a7d3b7c851386bf2db5e462535e96f72f714b03
matthews-crypto <78359171+matthews-crypto@users.noreply.github.com>
2022-11-05 11:36:26 +0000

    beta3

--- projet java/site.js
+++ projet java/site.js
@@ -1,2 +1,17 @@
-// carousel js
-// fin carousel
+// image qui defilent
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
